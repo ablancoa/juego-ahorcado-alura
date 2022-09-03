6ac2cc28791df8d61729df5a95362b16bfbb4a9e
Alexander <ablancoa95@gmail.com>
2022-09-03 16:41:10 -0400

    Obtener eventos del teclado

--- script_game.js
+++ script_game.js
@@ -1,26 +1,21 @@
 // Pagina del juego
 const newGame = document.querySelector('.btn-new-game');
 const inputContainer = document.querySelector('.input-container');
+const errorEntrado = document.querySelector('.letter-mistakes');
 
 newGame.addEventListener('click',selectWord);
 
+
+// Elegir variable al azar
 const arraySecretWords = ["hola","programa","laptop","codigo"];
+let arrayWord =[];
+
 function selectWord(){
     let secretWord = arraySecretWords[Math.floor(Math.random()*4)];
     console.log(secretWord);
-    createLines(secretWord);
+    arrayWord = startNewGame(secretWord);
 }
 
-function createLines(palabra){
-    let cantidad = Array.from(palabra);
-    cantidad.forEach(element => {
-        const letra = document.createElement('p')
-        letra.setAttribute('id',element);
-        letra.classList.add('input')
-        inputContainer.appendChild(letra);
-    });
-
-}
 // Array con los id de todas las letras
 let idLetras = function(){
     let letras = document.getElementsByClassName('input');
@@ -33,18 +28,74 @@ let idLetras = function(){
     
 }
 
+// Funcion de crear las letras
+function startNewGame(palabra){
+    clear()
+    let cantidad = Array.from(palabra);
+    let id = 0;
+    cantidad.forEach(element => {
+        const letra = document.createElement('p')
+        letra.setAttribute('id',`l${id}`);
+        letra.classList.add('input')
+        inputContainer.appendChild(letra);
+        id = id + 1
+    });
+    return cantidad;
+
+}
+
+// Funcion de limpiar pantalla
+function clear() {
+    let elementErase;
+    let idValue = idLetras();
+    console.log(idValue);
+    idValue.forEach(element => {
+        elementErase = document.getElementById(element);
+        inputContainer.removeChild(elementErase)
+    })
+    let letrasErase = document.getElementsByClassName('letters')
+    console.log(letrasErase);
+    while (letrasErase[0]){
+        letrasErase[0].parentNode.removeChild(letrasErase[0]);
+    }
+}
+
+
 let keyword = window.addEventListener('keypress', capturarLetra);
 
 function capturarLetra(event){
-    let captura = (String.fromCharCode(event.keyCode));
-    let nuemros = ["1","2","3","4","5","6","7","8","9","0"]
-    if (captura == (nuemros.find(element => element == captura))){
-        alert("Solo debe ingresar letras");
-    }
-    else if(typeof captura === 'string') {
-        console.log(captura);
+    if (event.keyCode == 13){
+        event.preventDefault();
     }
     else{
-        console.log("Solo debe ingresar letras");
+        let captura = String.fromCharCode(event.keyCode);
+        let nuemros = ["1","2","3","4","5","6","7","8","9","0"]
+        if (captura == (nuemros.find(element => element == captura))){
+            alert("Solo debe ingresar letras");
+        }
+        else if (captura == " " || captura == ""){
+            newGame.blur(); 
+            
+        }
+        else if(typeof captura === 'string') {
+            let compareId = idLetras();
+            let valueInput;
+            const letrasErrada =[]; 
+            if (arrayWord.includes(captura))
+            for (let i = 0; i < arrayWord.length; i++){
+                if (captura == arrayWord[i]){
+                    valueInput = document.getElementById(compareId[i]);
+                    valueInput.innerText = captura;
+                }
+            }
+            else{
+                const errorLetra = document.createElement('p');
+                errorLetra.classList.add('letters');
+                errorEntrado.appendChild(errorLetra)
+                errorLetra.innerText = captura;
+            }
+        }
+
     }
+    
 }
