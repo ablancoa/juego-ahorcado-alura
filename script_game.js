0b6f505234c57c618616b792c915190a9de90242
Alexander <ablancoa95@gmail.com>
2022-09-03 17:59:12 -0400

    Se corrigio el desempeno de imagenes

--- script_game.js
+++ script_game.js
@@ -2,8 +2,15 @@
 const newGame = document.querySelector('.btn-new-game');
 const inputContainer = document.querySelector('.input-container');
 const errorEntrado = document.querySelector('.letter-mistakes');
+const desistir = document.querySelector('.btn-surrender');
+let idImagenes = document.getElementsByClassName('munheco');
+
+let keyword = window.addEventListener('keypress', capturarLetra);
+const letrasErrada = [];
+let erroresCometidos = 0;
 
 newGame.addEventListener('click',selectWord);
+desistir.addEventListener('click',clear);
 
 
 // Elegir variable al azar
@@ -46,6 +53,16 @@ function startNewGame(palabra){
 
 // Funcion de limpiar pantalla
 function clear() {
+    // Para limpiar el muñeco
+    while (erroresCometidos != 0){
+        idImagenes[erroresCometidos-1].classList.add('inactive')
+        erroresCometidos--
+    }
+    // Para limpiar el array letrasErradas
+    while (letrasErrada[0]){
+        letrasErrada.pop()
+    }
+    // Para limpiar las letras correctas en pantalla
     let elementErase;
     let idValue = idLetras();
     console.log(idValue);
@@ -53,6 +70,7 @@ function clear() {
         elementErase = document.getElementById(element);
         inputContainer.removeChild(elementErase)
     })
+    // Para limpiar las letras equivocadas de pantalla
     let letrasErase = document.getElementsByClassName('letters')
     console.log(letrasErase);
     while (letrasErase[0]){
@@ -61,8 +79,6 @@ function clear() {
 }
 
 
-let keyword = window.addEventListener('keypress', capturarLetra);
-const letrasErrada = [];
 
 function capturarLetra(event){
     if (event.keyCode == 13){
@@ -98,6 +114,10 @@ function capturarLetra(event){
                     errorLetra.classList.add('letters');
                     errorEntrado.appendChild(errorLetra)
                     errorLetra.innerText = captura;
+                    erroresCometidos++    
+                    if(erroresCometidos != idImagenes.length){
+                        idImagenes[erroresCometidos-1].classList.remove('inactive')
+                    }
                 }
             }
         }
